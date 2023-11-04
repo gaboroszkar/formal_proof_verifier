b44d0bbdbe61443339832e5e1a3993aa133228c6
Gábor Oszkár Dénes <gaboroszkar@protonmail.com>
2023-11-04 23:18:43 +0100

    Add few example tests

--- tests/test_formal_proof_verifier.py
+++ tests/test_formal_proof_verifier.py
@@ -28,3 +28,46 @@ def test_multiple_reductio_ad_absurdum():
     lines: List[Union[str, Line]] = create_lines_from_text(text)
 
     assert all(line[1].is_valid() for line in lines)
+
+def test_principle_of_explosion_with_modus_tollens():
+    text: str = """
+        1    1 P&(~P)     A
+        2    2 ~Q         A
+        1    3 P          &E 1
+        1,2  4 P&(~Q)     &I 3,2
+        1,2  5 P          &E 4
+        1    6 (~Q)>P     CP 2,5
+        1    7 ~P         &E 1
+        1    8 ~(~Q)      MT 6,7
+        1    9 Q          DNE 8
+        -   10 (P&(~P))>Q CP 1,9
+    """
+
+    lines: List[Union[str, Line]] = create_lines_from_text(text)
+
+    assert all(line[1].is_valid() for line in lines)
+
+def test_reductio_ad_absurdum_with_modus_tollens():
+    text: str = """
+        1    1 P>(Q&(~Q))        A
+        2    2 P                 A
+        3    3 (P>P)             A
+        2,3  4 P&(P>P)           &I 2,3
+        2,3  5 P                 &E 4
+        2    6 (P>P)>P           CP 3,5
+        1,2  7 Q&(~Q)            MP 1,2
+        1,2  8 Q                 &E 7
+        1    9 P>Q               CP 2,8
+        1,2 10 ~Q                &E 7
+        1,2 11 ~P                MT 9,10
+        1,2 12 ~(P>P)            MT 6,11
+        1   13 P>(~(P>P))        CP 2,12
+        -   14 P>P               CP 2,2
+        -   15 ~(~(P>P))         DNI 14
+        1   16 ~P                MT 13,15
+        -   17 (P>(Q&(~Q)))>(~P) CP 1,16
+    """
+
+    lines: List[Union[str, Line]] = create_lines_from_text(text)
+
+    assert all(line[1].is_valid() for line in lines)
