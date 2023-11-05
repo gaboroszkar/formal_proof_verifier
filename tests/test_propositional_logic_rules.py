abe6f573ae1d8b959b526639bbbb81de91acf517
Gábor Oszkár Dénes <gaboroszkar@protonmail.com>
2023-11-05 08:33:22 +0100

    Add tests for &E rule

--- tests/test_propositional_logic_rules.py
+++ tests/test_propositional_logic_rules.py
@@ -90,3 +90,78 @@ def test_and_introduction_rule():
         1,2,3 6 (Q&R)&(Q&Q) &I 5,4
     """
     assert _map_is_valid(text) == [True, True, True, True, True, False]
+
+def test_and_elimination_rule():
+    # Valid use of the rule.
+    text: str = """
+        1 1 P&Q P
+        1 2 P   &E 1
+        1 3 Q   &E 1
+    """
+    assert _map_is_valid(text) == [True, True, True]
+
+    # Valid use of the rule.
+    text: str = """
+        1   1 Q&R         P
+        2   2 P&Q         P
+        1,2 3 (P&Q)&(Q&R) &I 2,1
+        1,2 4 P&Q         &E 3
+        1,2 5 P           &E 4
+        1,2 6 Q           &E 4
+        1,2 7 Q&R         &E 3
+        1,2 8 Q           &E 7
+        1,2 9 R           &E 7
+    """
+    assert _map_is_valid(text) == [True, True, True, True, True, True, True, True, True]
+
+    # Missing line numbers for the rule.
+    text: str = """
+        1 1 P&Q P
+        1 2 P   &E
+    """
+    with pytest.raises(RuntimeError):
+        create_lines_from_text(text)
+
+    # Too many line numbers for the rule.
+    text: str = """
+        1 1 P&Q P
+        2 2 R   P
+        1 3 P   &E 1,2
+    """
+    with pytest.raises(RuntimeError):
+        create_lines_from_text(text)
+
+    # Missing dependency number.
+    text: str = """
+        1   1 Q&R         P
+        2   2 P&Q         P
+        1,2 3 (P&Q)&(Q&R) &I 2,1
+        1   4 P&Q         &E 3
+        2   5 Q&R         &E 3
+    """
+    assert _map_is_valid(text) == [True, True, True, False, False]
+
+    # Extra invalid dependency number.
+    text: str = """
+        1     1 Q&R         P
+        2     2 P&Q         P
+        1,2   3 (P&Q)&(Q&R) &I 2,1
+        1,2,3 4 P&Q         &E 3
+    """
+    assert _map_is_valid(text) == [True, True, True, False]
+
+    # Incorrect connective.
+    text: str = """
+        1 1 PvQ P
+        1 2 P   &E 1
+    """
+    assert _map_is_valid(text) == [True, False]
+
+    # None of the formulas are the same.
+    text: str = """
+        1   1 Q&R         P
+        2   2 P&Q         P
+        1,2 3 (P&Q)&(Q&R) &I 2,1
+        1,2 4 P&P         &E 3
+    """
+    assert _map_is_valid(text) == [True, True, True, False]
