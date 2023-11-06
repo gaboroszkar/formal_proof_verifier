501872f5145a71f78001c863bece5b7f24ab74b4
Gábor Oszkár Dénes <gaboroszkar@protonmail.com>
2023-11-06 22:13:07 +0100

    Add tests for formula comparison

--- tests/test_formula.py
+++ tests/test_formula.py
@@ -1,9 +1,10 @@
 import pytest
 
-from formal_proof_verifier.formal_proof_verifier import create_formula, Formula, FormulaType
+from formal_proof_verifier.formal_proof_verifier import create_formula as cf
+from formal_proof_verifier.formal_proof_verifier import Formula, FormulaType
 
 def test_recognize_predicate():
-    formula: Formula = create_formula("P&Predicate(variable)")
+    formula: Formula = cf("P&Predicate(variable)")
     assert formula.type == FormulaType.and_type
     assert formula.left.type == FormulaType.atomic_type
     assert formula.left.atom == "P"
@@ -11,7 +12,7 @@ def test_recognize_predicate():
     assert formula.right.predicate == "Predicate"
     assert formula.right.variables == ["variable"]
 
-    formula: Formula = create_formula("Pv(Predicate)something")
+    formula: Formula = cf("Pv(Predicate)something")
     assert formula.type == FormulaType.or_type
     assert formula.left.type == FormulaType.atomic_type
     assert formula.left.atom == "P"
@@ -19,7 +20,7 @@ def test_recognize_predicate():
     assert formula.right.predicate == "Predicate"
     assert formula.right.variables == ["something"]
 
-    formula: Formula = create_formula("Tripredicate(v1,v2,v3)>P")
+    formula: Formula = cf("Tripredicate(v1,v2,v3)>P")
     assert formula.type == FormulaType.conditional_type
     assert formula.left.type == FormulaType.predicate_type
     assert formula.left.predicate == "Tripredicate"
@@ -27,7 +28,7 @@ def test_recognize_predicate():
     assert formula.right.type == FormulaType.atomic_type
     assert formula.right.atom == "P"
 
-    formula: Formula = create_formula("(a)is(b)vP")
+    formula: Formula = cf("(a)is(b)vP")
     assert formula.type == FormulaType.or_type
     assert formula.left.type == FormulaType.predicate_type
     assert formula.left.predicate == "is"
@@ -35,7 +36,7 @@ def test_recognize_predicate():
     assert formula.right.type == FormulaType.atomic_type
     assert formula.right.atom == "P"
 
-    formula: Formula = create_formula("(~(S={}))>(Ex(x)in(S))")
+    formula: Formula = cf("(~(S={}))>(Ex(x)in(S))")
     assert formula.type == FormulaType.conditional_type
     assert formula.left.type == FormulaType.not_type
     assert formula.left.inner.type == FormulaType.predicate_type
@@ -46,3 +47,29 @@ def test_recognize_predicate():
     assert formula.right.inner.type == FormulaType.predicate_type
     assert formula.right.inner.predicate == "in"
     assert formula.right.inner.variables == ["x", "S"]
+
+def test_formula_comparison():
+    assert cf("P") == cf("P")
+    assert cf("P&Q") == cf("P&Q")
+    assert cf("PvQ") == cf("PvQ")
+    assert cf("P>Q") == cf("P>Q")
+    assert cf("~P") == cf("~P")
+    assert cf("F(a,b,c)") == cf("F(a,b,c)")
+    assert cf("Ax(P&F(a,x))") == cf("Ay(P&F(a,y))")
+    assert cf("Ex(F(x,b)vQ)") == cf("Ey(F(y,b)vQ)")
+
+    assert cf("P") != cf("Q")
+    assert cf("P&Q") != cf("PvQ")
+    assert cf("P>Q") != cf("PvQ")
+    assert cf("~P") != cf("P")
+    assert cf("F(a,b,c)") != cf("G(a,b,c)")
+    assert cf("F(a,b,c)") != cf("F(a,b)")
+    assert cf("F(a,b,c)") != cf("F(a,b,d)")
+    assert cf("Ex(F(x))") != cf("Ay(F(y))")
+
+    assert cf("P&Q") != cf("R&Q")
+    assert cf("PvQ") != cf("PvR")
+    assert cf("~P") != cf("~Q")
+
+    assert cf("Ax(P&F(a,x))") != cf("Ay(P&F(a,x))")
+    assert cf("Ex(F(x,b)vQ)") != cf("Ey(F(x,b)vQ)")
