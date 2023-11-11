2e8e0e549a0b40965736d1d9248cf4c7243d96f5
Gábor Oszkár Dénes <gaboroszkar@protonmail.com>
2023-11-11 11:19:48 +0100

    Extend equality introduction rules for covarage

--- tests/test_equality_rules.py
+++ tests/test_equality_rules.py
@@ -17,3 +17,33 @@ def test_equality_introduction_rule():
     """
     with pytest.raises(RuntimeError):
         create_lines_from_text(text)
+
+    # Too many line numbers for the rule.
+    text: str = """
+        1 1 a=a =I
+    """
+    assert map_is_valid(text) == [False]
+
+    # Incorrect formula type.
+    text: str = """
+        - 1 P&P =I
+    """
+    assert map_is_valid(text) == [False]
+
+    # Incorrect predicate.
+    text: str = """
+        - 1 F(a,a) =I
+    """
+    assert map_is_valid(text) == [False]
+
+    # Incorrect number of variables for the predicate.
+    text: str = """
+        - 1 =(a,a,a) =I
+    """
+    assert map_is_valid(text) == [False]
+
+    # Not the same variables on both sides.
+    text: str = """
+        - 1 a=b =I
+    """
+    assert map_is_valid(text) == [False]
